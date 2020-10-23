89d8dd6a38c24146f777c149bf8337021cc68e7f
John Sultana <John.Sultana.d53222@mcast.edu.mt>
2020-10-23 11:44:13 +0200

    Lesson 2 - Intro to Objects and Constructors

---
+++ OOP/Worksheet1/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet1
+{
+    class Circle
+    {
+        //constructor bears the same name as the class which is used to create an instance of a class(the object)
+        //  secondary objective of a construct: is can be used as a normal method
+        public Circle()
+        {
+            Radius = 0; //setting to default values
+            Colour = "Black";
+        }
+
+        public string Colour { get; set; }
+        public double Radius { get; set; } //property
+
+        public string GetRadius()
+        {
+            return "Radius: " + Radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public double GetPerimiter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
---
+++ OOP/Worksheet1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object instance -> object is when you create a space in memory and you start assigning values to the template you created earlier
+            Circle c = new Circle(); //example of an object --> c
+
+            Console.Write("Input a radius: ");
+            c.Radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Area is {Math.Round(c.GetArea(), 2)}, Perimiter is {Math.Round(c.GetPerimiter(), 2)}");
+
+            Console.WriteLine("Press a key to teminate...");
+            Console.ReadKey();
+        }
+    }
+}
