106b0e254e845cfb27e27a503335ed3b427ef914
John Sultana <John.Sultana.d53222@mcast.edu.mt>
2020-10-23 10:55:57 +0200

    Lesson 1 Introduction to OOP (classes, fields, properties, methods)

---
+++ OOP/Lesson1_IntroToOOP/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_IntroToOOP
+{
+    //class: is a blueprint (template) that defines an entity structure. An entity (useally) represents a real life model
+    //object which is described using characteristics
+    //use Pascal casing for class names and properties
+    //field: is a variable which describes something about the model/object
+    //use camel casing for field names
+    //property is (usually) declared as public and controls the accessibility to the field
+    class Person //an example of a class
+    {
+        string idCardNo;
+        private string name; //an example of a field
+        string surname;
+        DateTime dateOfBirth;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string IdCard
+        {
+            get { return idCardNo; }
+            set {
+                switch (value.ToString().Length)
+                {
+                    case 5:
+                        idCardNo = "00" + value;
+                        break;
+                    case 6:
+                        idCardNo = "0" + value;
+                        break;
+                    default:
+                        idCardNo = value.ToString();
+                        break;
+                }
+
+                idCardNo = value.ToString().ToUpper();
+
+            }//JGE63G
+        }
+
+        public string FullName //read only property
+        {
+            get { return Name + " " + Surname; }
+        }
+
+        public string Surname { get => surname; set => surname = value; }
+
+        public string Address { get; set; }
+        public string MobileNo { get; set; }
+        public string TelNo { get; set; }
+
+        public DateTime DateOfBirth { get; set;}
+
+        public int CalculateAge()
+        {
+            DateTime today = DateTime.Today;
+            return today.Year - DateOfBirth.Year;
+        }
+
+        public string DeLimitAddress(char c)
+        {
+            string[] delimitedAddress = Address.Split(new char[] { ' ' });
+            string output = "";
+            foreach(string s in delimitedAddress)
+            {
+                output += s + c + " ";
+            }
+            return output.Remove(output.Length - 2);
+        }
+    }
+}
---
+++ OOP/Lesson1_IntroToOOP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_IntroToOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            Console.WriteLine("Please input Address: ");
+            p.Address = Console.ReadLine();
+
+            Console.WriteLine(p.DeLimitAddress(','));
+            Console.WriteLine("Press a key to teminate app...");
+            Console.ReadKey();
+
+            /*
+
+            p.IdCard = "23363G"; //write
+
+            p.Name = "John";
+            p.Surname = "Sultana";
+
+            Console.WriteLine($"{p.FullName}");
+            Console.ReadKey();*/
+        }
+    }
+}
